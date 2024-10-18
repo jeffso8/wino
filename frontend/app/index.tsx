8ba01cf6cb1b076b610918e73ebb5c1eeb9a2cb9
Scott So <scottso@scotts-mbp-2.lan>
2024-10-17 20:06:14 -0700

    react router`

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,7 +1,17 @@
-import { Redirect } from 'expo-router';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Welcome from "./Welcome";
+
+const Stack = createNativeStackNavigator();
 
 const Home = () => {
-  return <Redirect href="/(auth)/welcome" />;
-}
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Welcome" component={Welcome} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
 
 export default Home;
